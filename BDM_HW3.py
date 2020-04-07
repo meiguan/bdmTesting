c5f55a6ab7145a28b960b30181f5b245258c7474
mei guan <yguan66@gmail.com>
2020-04-07 15:48:15 -0400

    add to csv

--- BDM_HW3.py
+++ BDM_HW3.py
@@ -2,13 +2,16 @@ import sys
 import csv
 from pyspark import SparkContext
 
+def tocsv(data):
+    return ','.join(str(d) for d in data)
+
 if __name__=='__main__':
-    file = sys.argv[0]
-    output = sys.argv[1]
+    file = sys.argv[1]
+    output = sys.argv[2]
     sc = SparkContext()
     sc.textFile(file) \
     .mapPartitions(lambda partition: csv.reader([line.replace('\n','') for line in partition],delimiter=',', quotechar='"')) \
-    .filter(lambda line: len(line) > 7 and line[0] != 'Date received' and len(line[0])==10) \
+    .filter(lambda line: len(line) > 6 and line[0] != 'Date received' and len(line[0])==10) \
     .map(lambda line: ((line[1], line[0][:4], line[7]), 1)) \
     .groupByKey() \
     .mapValues(lambda values: sum(values)) \
@@ -17,4 +20,5 @@ if __name__=='__main__':
     .mapValues(lambda x: (sum(x), len(x), round(max(x)*100/sum(x)))) \
     .sortByKey() \
     .map(lambda line: (line[0][0], line[0][1], line[1][0], line[1][1], line[1][2])) \
+    .map(tocsv) \
     .saveAsTextFile(output)
